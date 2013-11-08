95b80f550e09f07c51c54eb788a038a6358d8c9b
adius <mail@adriansieber.com>
2013-11-08 14:51:23 +0100

    Added testserver, included stream-graph

---
+++ js/simterm.js
@@ -0,0 +1,132 @@
+!function () {
+
+	var svg,
+		area,
+		y,
+		x,
+		width, height,
+		color,
+		n = 20, // number of layers
+		m = 200, // number of samples per layer
+		stack = d3.layout.stack().offset("wiggle")
+		/*layers0 = stack(d3.range(n).map(function () {
+			return bumpLayer(m)
+		})),
+		layers1 = stack(d3.range(n).map(function () {
+			return bumpLayer(m)
+		}))*/
+
+
+	function renderStreamGraph(data){
+
+
+		width = 960
+		height = 500
+
+		x = d3
+			.scale
+			.linear()
+			.domain([0, m - 1])
+			.range([0, width])
+
+		y = d3
+			.scale
+			.linear()
+			.domain([0, d3.max(layers0.concat(layers1), function (layer) {
+				return d3.max(layer, function (d) {
+					return d.y0 + d.y
+				})
+			})])
+			.range([height, 0])
+
+		color = d3
+			.scale
+			.linear()
+			.range(["rgb(255,0,0)", "rgb(0,0,255)"])
+
+		area = d3
+			.svg
+			.area()
+			.x(function (d) {
+				return x(d.x)
+			})
+			.y0(function (d) {
+				return y(d.y0)
+			})
+			.y1(function (d) {
+				return y(d.y0 + d.y)
+			})
+
+		svg = d3
+			.select("body")
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height)
+
+		svg
+			.selectAll("path")
+			.data(layers0)
+			.enter()
+			.append("path")
+			.attr("d", area)
+			.style("fill", function () {
+				return color(Math.random())
+			})
+
+		function transition() {
+			d3
+				.selectAll("path")
+				.data(function () {
+					var d = layers1
+
+					layers1 = layers0
+					return layers0 = d
+				})
+				.transition()
+				.duration(2500)
+				.attr("d", area)
+		}
+
+		// Inspired by Lee Byron's test data generator.
+		function bumpLayer(n) {
+
+			function bump(a) {
+
+				var x = 1 / (.1 + Math.random()),
+					y = 2 * Math.random() - .5,
+					z = 10 / (.1 + Math.random())
+
+				for (var i = 0; i < n; i++) {
+
+					var w = (i / n - y) * z
+
+					a[i] += x * Math.exp(-w * w)
+				}
+			}
+
+			var a = [],
+				i
+
+			for (i = 0; i < n; ++i) a[i] = 0
+
+			for (i = 0; i < 5; ++i) bump(a)
+
+			console.log(a)
+
+			return a.map(function (d, i) {
+				return {x: i, y: Math.max(0, d)}
+			})
+		}
+	}
+
+	$.ajax({
+		url: 'http://localhost:1234/simterm',
+		success: function(data){
+			console.log(data)
+		},
+		error: function(data){
+			console.error(data)
+		}
+
+	})
+}()
---
+++ server.js
@@ -0,0 +1,141 @@
+var restify = require('restify'),
+	fakesome = require('./node_modules/fakesome/fakesome.js'),
+	server = restify.createServer({
+		name: 'TestServer',
+		version: '0.0.1'
+	})
+
+/*function checkEquality(obj1, obj2) {
+
+ if (Object.keys(obj1).length !== Object.keys(obj1).length)
+ return false
+
+ for (var i in obj1)
+ if (obj1.hasOwnProperty(i) &&
+ (!obj2.hasOwnProperty(i) || obj1[i] !== obj2[i]))
+ return false
+
+ return true
+ }*/
+
+
+server.use(restify.acceptParser(server.acceptable))
+server.use(restify.queryParser())
+server.use(restify.bodyParser())
+server.use(restify.gzipResponse())
+server.use(restify.CORS())
+
+
+/*
+map.forEach(function (item) {
+
+	server.get(baseURL + '/' + item.url, function (req, res, next) {
+
+
+		var success = item.queries.some(function (query) {
+
+				if (checkEquality(query.query, req.query)) {
+					res.send(require('./testdata/' + query.file))
+					return true
+				}
+
+				return false
+			}),
+			errorString = 'No data for the query ' +
+				JSON.stringify(req.query) + ' available'
+
+		if (!success)
+			return next(new restify.ResourceNotFoundError(errorString))
+		else
+			return next()
+	})
+})
+*/
+
+
+server.get('/simterm', function (req, res, next) {
+
+	function bumpLayer(n) {
+
+		function bump(a) {
+
+			var x = 1 / (0.1 + Math.random()),
+				y = 2 * Math.random() - .5,
+				z = 10 / (0.1 + Math.random())
+
+			for (var i = 0; i < n; i++) {
+
+				var w = (i / n - y) * z
+
+				a[i] += x * Math.exp(-w * w)
+			}
+		}
+
+		var a = [],
+			i
+
+		for (i = 0; i < n; ++i)
+			a[i] = 0
+
+		for (i = 0; i < 5; ++i)
+			bump(a)
+
+
+		return a.map(function (d, i) {
+			return {x: i, y: Math.max(0, d)}
+		})
+	}
+
+
+	var test = {
+		"term": "SAP",
+		"associations": {
+			"2013-10-10T22:00": [
+				{
+					name: "Hana",
+					value: "0.4"
+				},
+				{
+					name: "Hasso",
+					value: "0.2"
+				}
+			],
+
+			"2013-10-11T22:00": [
+				{
+					name: "Hana",
+					value: "0.4"
+				},
+				{
+					name: "Hasso",
+					value: "0.2"
+				}
+
+			]
+		}
+	}
+
+	test = fakesome.object({
+		"term": "word()",
+		"associations": fakesome.array(100).object({
+			time: "2013-10-10T22:00",
+			associations: [
+				{
+					name: "Hana",
+					value: "0.4"
+				},
+				{
+					name: "Hasso",
+					value: "0.2"
+				}
+			]
+		})
+	})
+
+	res.send(test)
+})
+
+
+server.listen(1234, function () {
+	console.log('%s listening at %s', server.name, server.url)
+})
