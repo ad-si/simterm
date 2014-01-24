866f0a144193cb9f8f7de7a8b7cfb281bafe2c0d
adius <mail@adriansieber.com>
2014-01-24 15:07:16 +0100

    Switched to native date-picker

--- js/index.js
+++ js/index.js
@@ -2,26 +2,28 @@
 
 
 	/*  ToDO:   - show timescale under steamgraph
-				- fix artefacts
-				- field to choose start and end datum
-				- intelligent scaling
+	 - fix artefacts
+	 - field to choose start and end datum
+	 - intelligent scaling
 	 */
 
 	var $rangeSlider = $("#rangeSlider"),
 		searchValue = 'Apple',
 		firstLoad = true,
 
-
 	//TODO: get max and min values from dataset
 		min = new Date('2000-03-03').getTime(),
 		max = new Date('2013-12-10').getTime()
 
-		minTime.value = new Date(min).toJSON().substr(0,10)
-		maxTime.value = new Date(max).toJSON().substr(0,10)
+	//minTime.value = new Date(min).toJSON().substr(0,10)
+	//maxTime.value = new Date(max).toJSON().substr(0,10)
 
 	//TODO: write min and max to slider
 
 	function setSliderLabels(lower, upper) {
+
+		var $upperSliderlabel = $rangeSlider.find('.noUi-handle-upper .sliderLabel'),
+			$lowerSliderlabel = $rangeSlider.find('.noUi-handle-lower .sliderLabel')
 
 		if (firstLoad) {
 
@@ -39,30 +41,33 @@
 		}
 		else {
 
-			$rangeSlider
-				.find('.noUi-handle-lower .sliderLabel')
-				.attr('data-content', lower.substr(0, 10))
+			$lowerSliderlabel.attr('data-content', lower.substr(0, 10))
 
-			$rangeSlider
-				.find('.noUi-handle-upper .sliderLabel')
-				.attr('data-content', upper.substr(0, 10))
+			$upperSliderlabel.attr('data-content', upper.substr(0, 10))
 		}
+
+		$lowerSliderlabel.click(alert)
+		$upperSliderlabel.click(alert)
 
 		firstLoad = false
 	}
 
 
-	function loadData(){
-		simterm.loadData(function(data){
-			simterm
-				.data(data)
-				.render()
-		}, {query: {
-			keywords: searchValue,
-			from: new Date(Number($rangeSlider.val()[0])).toJSON(),
-			to: new Date(Number($rangeSlider.val()[1])).toJSON()
-		}
-		} )
+	function loadData() {
+		simterm.loadData(
+			function (data) {
+				simterm
+					.data(data)
+					.render()
+			},
+			{
+				query: {
+					keywords: searchValue,
+					from: new Date(Number($rangeSlider.val()[0])).toJSON(),
+					to: new Date(Number($rangeSlider.val()[1])).toJSON()
+				}
+			}
+		)
 	}
 
 
@@ -84,71 +89,106 @@
 			var from = new Date(Number($rangeSlider.val()[0])).toJSON(),
 				to = new Date(Number($rangeSlider.val()[1])).toJSON()
 
-			simterm.loadData(function(data){
-				simterm
-					.data(data)
-					.render()
+			simterm.loadData(function (data) {
+					simterm
+						.data(data)
+						.render()
 
-				$('#waity').hide()
-			}, {query: {keywords: searchValue, from: from, to: to}}
+					$('#waity').hide()
+				}, {query: {keywords: searchValue, from: from, to: to}}
 			)
 		}
 	})
 
 	setSliderLabels(new Date(min).toJSON(), new Date(max).toJSON())
 
-	keywords.addEventListener('keypress', function(event){
-		if( event.keyCode == 13){
+	keywords.addEventListener('keypress', function (event) {
+		if (event.keyCode == 13) {
 			searchValue = keywords.value
 			loadData()
 		}
 	}, false)
 
 
-	$("#minTime").datepicker({
-		changeYear: true,
-		changeMonth: true,
-		numberOfMonths: 1,
-		maxDate: new Date(max-1),
-		dateFormat: "dd.mm.yy",
-		onSelect: function (selectedDate) {
-			min = new Date(selectedDate).getTime()
-			minTime.value = new Date(selectedDate).toJSON().substr(0, 10)
-			firstLoad = true
-			$rangeSlider.noUiSlider({
-				range: [min, max]
-			}, true)
+	function setSliderMinMax() {
 
-			if(new Date(selectedDate).getTime() > Number($rangeSlider.val()[0]))
-			{
-				setSliderLabels(new Date(min).toJSON(), new Date(max).toJSON())
-				loadData()
-			}
-		}
-	})
+		var min = $('#minTime').val(),
+			max = $('#maxTime').val()
 
-	$("#maxTime").datepicker({
-		changeYear: true,
-		changeMonth: true,
-		numberOfMonths: 1,
-		minDate: new Date(min+1),
-		maxDate: 0,
-		dateFormat: "dd.mm.yy",
-		onSelect: function (selectedDate) {
-			max = new Date(selectedDate).getTime()
-			maxTime.value = new Date(selectedDate).toJSON().substr(0, 10)
-			firstLoad = true
-			$rangeSlider.noUiSlider({
-				range: [min, max]
-			}, true)
+		firstLoad = true
 
-			if(new Date(selectedDate).getTime() < Number($rangeSlider.val()[1]))
-			{
-				setSliderLabels(new Date(min).toJSON(), new Date(max).toJSON())
-				loadData()
-			}
-		}
-	})
+		$rangeSlider.noUiSlider({
+			range: [new Date(min).getTime(), new Date(max).getTime()]
+		}, true)
+
+		//if(new Date(selectedDate).getTime() > Number($rangeSlider.val()[0])){
+
+		setSliderLabels(new Date(min).toJSON(), new Date(max).toJSON())
+		loadData()
+
+		//}
+
+	}
+
+
+	$("#minTime")
+		.attr('value', '2000-03-03')
+		.attr('min', '2000-03-03')
+		.attr('max', '2013-12-10')
+		.on('change', setSliderMinMax)
+
+	$("#maxTime")
+		.attr('value', '2013-12-10')
+		.attr('min', '2000-03-03')
+		.attr('max', '2013-12-10')
+		.on('change', setSliderMinMax)
+
+	/*
+	 $("#minTime").datepicker({
+	 changeYear: true,
+	 changeMonth: true,
+	 numberOfMonths: 1,
+	 maxDate: new Date(max-1),
+	 dateFormat: "dd.mm.yy",
+	 onSelect: function (selectedDate) {
+	 min = new Date(selectedDate).getTime()
+	 minTime.value = new Date(selectedDate).toJSON().substr(0, 10)
+	 firstLoad = true
+	 $rangeSlider.noUiSlider({
+	 range: [min, max]
+	 }, true)
+
+	 if(new Date(selectedDate).getTime() > Number($rangeSlider.val()[0]))
+	 {
+	 setSliderLabels(new Date(min).toJSON(), new Date(max).toJSON())
+	 loadData()
+	 }
+	 }
+	 })
+
+	 $("#maxTime").datepicker({
+	 changeYear: true,
+	 changeMonth: true,
+	 numberOfMonths: 1,
+	 minDate: new Date(min+1),
+	 maxDate: 0,
+	 dateFormat: "dd.mm.yy",
+	 onSelect: function (selectedDate) {
+	 max = new Date(selectedDate).getTime()
+	 maxTime.value = new Date(selectedDate).toJSON().substr(0, 10)
+	 firstLoad = true
+	 $rangeSlider.noUiSlider({
+	 range: [min, max]
+	 }, true)
+
+	 if(new Date(selectedDate).getTime() < Number($rangeSlider.val()[1]))
+	 {
+	 setSliderLabels(new Date(min).toJSON(), new Date(max).toJSON())
+	 loadData()
+	 }
+	 }
+	 })
+	 */
 
 
 	$('#filters')
@@ -239,7 +279,7 @@
 	})
 
 
-simterm.loadData(function (data) {
+	simterm.loadData(function (data) {
 		simterm
 			.data(data)
 			.render()
